fdd849c18b7b1c4aac7aa9c8407019be0a0a0faf
Filip Trcka <filiptrcka@gmail.com>
2022-10-24 10:35:34 +0200

    Made the calculator !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Insurance-calculator';
+
+
+  inboundClick = true || false;
+  ClickHideButton = true || false;
+  
+
+  toShowSumarTotal = '';
+  toShowSumar = '';
+  toSumar = '';
+  toShow = '';
+  curVal = '';
+  curValSpec = '';
+  curValSpecTotal = '';
+  
+  clear() {
+    this. toShowSumarTotal = '';
+    this.toShowSumar = '';
+    this.toSumar = '';
+    this.toShow = '';
+    this.curVal = '';
+    this.curValSpec = '';
+    this.curValSpecTotal = '';
+  }
+
+  back(){
+    this.curVal = this.curVal.slice(0,-1)
+    this.toShow = this.curVal
+  }
+  back1(){
+    this.toShowSumar = '';
+  }
+
+  back2(){
+    this. toShowSumarTotal = '';
+  }
+
+  writeToInput(value?: any) {
+    this.curVal = this.curVal + value;
+    this.toShow = this.curVal;
+  }
+
+  show() {
+    this.toShow = eval(this.curVal);
+    this.curVal = this.toShow;
+  }
+
+  writeToInputSpec(value?: string) {
+    this.curValSpec = this.curVal + value;
+    this.toShowSumar = this.curValSpec;
+  }
+
+  showSumar() {
+    this.toShowSumar = eval(this.curValSpec);
+    this.curValSpec = this.toShowSumar;
+  }
+
+  inputCelkomshow(value?: string) {
+    this.curValSpecTotal = this.curValSpec + this.curVal ;
+    this.toShowSumarTotal = this.curValSpecTotal;
+  }
+
 }
